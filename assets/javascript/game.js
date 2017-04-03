bd42b40d00c4612711bb33f881d4a6fa91cca2f4
Omar I <omar@kineticvortex.com>
2017-04-02 23:33:54 -0500

    Add HTML, CSS and JS into their respective pages.

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+var wins = 0;
+var losses = 0;
+var playerNumber = 0;
+var goalNumber = 0;
+var berry1 = 0;
+var berry2 = 0;
+var berry3 = 0;
+var berry4 = 0;
+
+
+$(document).ready(function() {
+  $("#goal-number").html(Math.floor(Math.random() * ))
+});
