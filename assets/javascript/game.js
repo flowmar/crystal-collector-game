371292d2535b531387a5cec67143d15af05fb244
Omar I <omar@kineticvortex.com>
2017-04-06 11:31:38 -0500

    add functionality to log wins and losses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -31,10 +31,10 @@ $(document).ready(function() {
     goalNumber = Math.floor(Math.random() * 120) + 19;
 
   // Grab the element with id goal-number and insert goal
-    $( "#goal-number" ).html("<p id=\"x-concentration-text\"> Chemical X Concentration Goal: <span id=\"goal-number-text\">" + goalNumber + "</span></p>");
+    $( "#goal-number" ).html("<p id=\"x-concentration-text\"> Chemical X Level to Reach: <span id=\"goal-number-text\">" + goalNumber + "</span></p>");
 
   // Grab elements with class 'wins-losses' and display variable wins and variable losses
-    $( ".wins-losses" ).html(" <p> Wins: " + wins + "</p><br> <p>Losses:" + losses + "</p>");
+    $( ".wins-losses" ).html("<div> <div class='wins'> Wins: " + wins + "</div><br> <div class='losses'>Losses:" + losses + "</div>");
 
   // Grab elements with class 'current-number' and insert html to display current number
     $( ".current-number" ).html(" <p> Your Current Level of Chemical X: " + playerNumber +"</p>");
@@ -46,30 +46,42 @@ $(document).ready(function() {
       // and update the displayed playerNumber
         $(".current-number").text("Your current Level of Chemical X: " + playerNumber);
 
-      // If player wins or loses based on last click, perform specific actions
+      // If player loses based on last click, display losing messages
         if (playerNumber > goalNumber) {
             $("#goal-number").html("<p class=\"loser\"> You suffered a <em>very</em> uncomfortble fate...  </p>");
             losses += 1;
+            $(".losses").html("<p>" + losses + "</p>");
             alert("death");
         }
+      // If player wins based on last click, display winning messages
         else if (playerNumber === goalNumber) {
             $("#goal-number").html("<p class=\"winner\"> You're Cured!!</p>");
             wins += 1;
+            $(".wins").html("<p>Wins : " + wins + "</p>");
             alert("You won!");
         }
     });
+
     // Repeat for all 4 Berries
+
+    // On click, add berry value to playerNumber and update display
     $("#berry2").on("click", function(){
         playerNumber += berry2;
         $(".current-number").text("Your current Level of Chemical X: " + playerNumber);
+
+    // If player loses based on last click, display losing message.
         if (playerNumber > goalNumber) {
             $("#goal-number").html("<p class=\"loser\"> You suffered a <em>very</em> uncomfortble fate...  </p>");
             losses += 1;
+            $(".losses").html("<p>" + losses + "</p>");
             alert("death");
         }
+
+    // Otherwise, if the player wins based on last click, display winning messages
         else if (playerNumber === goalNumber) {
             $("#goal-number").html("<p class=\"winner\"> You're Cured!!</p>");
             wins += 1;
+            $(".wins").html("<p>Wins : " + wins + "</p>");
             alert("You won!");
         }
     });
@@ -80,26 +92,30 @@ $(document).ready(function() {
         if (playerNumber > goalNumber) {
             $("#goal-number").html("<p class=\"loser\"> You suffered a <em>very</em> uncomfortble fate...  </p>");
             losses += 1;
+            $(".losses").html("<p>" + losses + "</p>");
             alert("death");
         }
         else if (playerNumber === goalNumber) {
             $("#goal-number").html("<p class=\"winner\"> You're Cured!!</p>");
             wins += 1;
+            $(".wins").html("<p>Wins : " + wins + "</p>");
             alert("You won!");
         }
     });
 
     $("#berry4").on("click", function(){
         playerNumber += berry4;
-        $(".current-number").text("Your current Level of Chemical X: " + playerNumber);
+        $(".current-number").text("Your current Level of Chemical X:" + playerNumber);
         if (playerNumber > goalNumber) {
             $("#goal-number").html("<p class=\"loser\"> You suffered a <em>very</em> uncomfortble fate...  </p>");
             losses += 1;
+            $(".losses").html("<p>Losses: " + losses + "</p>");
             alert("death");
         }
         else if (playerNumber === goalNumber) {
             $("#goal-number").html("<p class=\"winner\"> You're Cured!!</p>");
             wins += 1;
+            $(".wins").html("<p>Wins : " + wins + "</p>");
             alert("You won!");
         }
     });
