fb5157b0705057a6d200befa28a2831e8982a408
Omar I <omar@kineticvortex.com>
2017-04-05 22:11:02 -0500

    make changes in js and add .gitignore

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,4 @@
+// Initialize variables
 var wins = 0;
 var losses = 0;
 var playerNumber = 0;
@@ -7,15 +8,33 @@ var berry2 = 0;
 var berry3 = 0;
 var berry4 = 0;
 
-
+// When document is ready...
 $(document).ready(function() {
+  // Set each berry to a random value between 1 and 12.
     berry1 = Math.floor(Math.random() * 12) + 1;
     berry2 = Math.floor(Math.random() * 12) + 1;
     berry3 = Math.floor(Math.random() * 12) + 1;
     berry4 = Math.floor(Math.random() * 12) + 1;
+  // Declare goal variable and set it to a random number between 19 and 120.
     var goalNumber = Math.floor(Math.random() * 120) + 19;
-    $("#goal-number").html('<p style="font-size: 2em; color: white"> Chemical X Concentration Goal: <span id="goal-number-text">' + goalNumber + "</span></p>");
+
+    $("#goal-number").html('<p id="x-concentration-text"> Chemical X Concentration Goal: <span id="goal-number-text">' + goalNumber + "</span></p>");
     $(".wins-losses").html(' <p> Wins: ' + wins + '</p><br> <p>Losses:' + losses + '</p');
     $(".current-number").html(' <p> Your Current Level of Chemical X: ' + playerNumber +'</p>');
+    $('#berry1').on('click', 'button', function() {
+      $(this).animate({"top": "-10px"});
+    })
+    $('#berry1').on('click', function(){
+      playerNumber += berry1;
+    });
+     $('#berry2').on('click', function(){
+      playerNumber += berry2;
+    });
+      $('#berry3').on('click', function(){
+      playerNumber += berry3;
+    });
+       $('#berry4').on('click', function(){
+      playerNumber += berry4;
+    });
 });
 
