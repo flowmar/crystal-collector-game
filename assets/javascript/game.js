784772b2073020a57b28e5a6ecf4c748581525c0
Omar I <omar@kineticvortex.com>
2017-04-05 21:36:41 -0500

    add more js, html and css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,7 +9,13 @@ var berry4 = 0;
 
 
 $(document).ready(function() {
-  var goalNumber = Math.floor(Math.random() * 120) - 19;
-  $("#goal-number").html('<p><strong> Goal: ' + goalNumber + '</strong></p>';)
+    berry1 = Math.floor(Math.random() * 12) + 1;
+    berry2 = Math.floor(Math.random() * 12) + 1;
+    berry3 = Math.floor(Math.random() * 12) + 1;
+    berry4 = Math.floor(Math.random() * 12) + 1;
+    var goalNumber = Math.floor(Math.random() * 120) + 19;
+    $("#goal-number").html('<p style="font-size: 2em; color: white"> Chemical X Concentration Goal: <span id="goal-number-text">' + goalNumber + "</span></p>");
+    $(".wins-losses").html(' <p> Wins: ' + wins + '</p><br> <p>Losses:' + losses + '</p');
+    $(".current-number").html(' <p> Your Current Level of Chemical X: ' + playerNumber +'</p>');
 });
 
