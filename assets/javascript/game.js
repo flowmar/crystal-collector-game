5969c23147212ef504efc58db608acc6b0c8732b
Omar I <omar@kineticvortex.com>
2017-04-06 09:39:42 -0500

    fix infinite loop

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,11 +2,22 @@
 var wins = 0;
 var losses = 0;
 var playerNumber = 0;
-var goalNumber = 0;
+var goalNumber = 2;
 var berry1 = 0;
 var berry2 = 0;
 var berry3 = 0;
 var berry4 = 0;
+
+if (playerNumber > goalNumber) {
+    $("#goal-number").html("<p class=\"loser\"> You suffered a <em>very</em> uncomfortble fate...  </p>");
+    losses += 1;
+    alert("death");
+}
+else if (playerNumber === goalNumber) {
+    $("#goal-number").html("<p class=\"winner\"> You're Cured!!</p>");
+    wins += 1;
+    alert("You won!");
+}
 
 // When document is ready...
 $(document).ready(function() {
@@ -17,46 +28,84 @@ $(document).ready(function() {
     berry4 = Math.floor(Math.random() * 12) + 1;
 
   // Declare goal variable and set it to a random number between 19 and 120.
-    var goalNumber = Math.floor(Math.random() * 120) + 19;
+    goalNumber = Math.floor(Math.random() * 120) + 19;
 
   // Grab the element with id goal-number and insert goal
-    $( "#goal-number" ).html('<p id="x-concentration-text"> Chemical X Concentration Goal: <span id="goal-number-text">' + goalNumber + "</span></p>");
+    $( "#goal-number" ).html("<p id=\"x-concentration-text\"> Chemical X Concentration Goal: <span id=\"goal-number-text\">" + goalNumber + "</span></p>");
 
   // Grab elements with class 'wins-losses' and display variable wins and variable losses
-    $( ".wins-losses" ).html(' <p> Wins: ' + wins + '</p><br> <p>Losses:' + losses + '</p>');
+    $( ".wins-losses" ).html(" <p> Wins: " + wins + "</p><br> <p>Losses:" + losses + "</p>");
 
   // Grab elements with class 'current-number' and insert html to display current number
-    $( ".current-number" ).html(' <p> Your Current Level of Chemical X: ' + playerNumber +'</p>');
+    $( ".current-number" ).html(" <p> Your Current Level of Chemical X: " + playerNumber +"</p>");
 
-    do {
   // When the berries are clicked, add the value that the berry contains to the playerNumber variable
-  // and update the displayed playerNumber
-    $('#berry1').on('click', function(){
+    $("#berry1").on("click", function(){
         playerNumber += berry1;
-        $('.current-number').text("Your current Level of Chemical X: " + playerNumber);
-    });
 
-     $('#berry2').on('click', function(){
+      // and update the displayed playerNumber
+        $(".current-number").text("Your current Level of Chemical X: " + playerNumber);
+
+      // If player wins or loses based on last click, perform specific actions
+        if (playerNumber > goalNumber) {
+            $("#goal-number").html("<p class=\"loser\"> You suffered a <em>very</em> uncomfortble fate...  </p>");
+            losses += 1;
+            alert("death");
+        }
+        else if (playerNumber === goalNumber) {
+            $("#goal-number").html("<p class=\"winner\"> You're Cured!!</p>");
+            wins += 1;
+            alert("You won!");
+        }
+    });
+    // Repeat for all 4 Berries
+    $("#berry2").on("click", function(){
         playerNumber += berry2;
-        $('.current-number').text("Your current Level of Chemical X: " + playerNumber);
+        $(".current-number").text("Your current Level of Chemical X: " + playerNumber);
+        if (playerNumber > goalNumber) {
+            $("#goal-number").html("<p class=\"loser\"> You suffered a <em>very</em> uncomfortble fate...  </p>");
+            losses += 1;
+            alert("death");
+        }
+        else if (playerNumber === goalNumber) {
+            $("#goal-number").html("<p class=\"winner\"> You're Cured!!</p>");
+            wins += 1;
+            alert("You won!");
+        }
     });
 
-      $('#berry3').on('click', function(){
+    $("#berry3").on("click", function(){
         playerNumber += berry3;
-        $('.current-number').text("Your current Level of Chemical X: " + playerNumber);
+        $(".current-number").text("Your current Level of Chemical X: " + playerNumber);
+        if (playerNumber > goalNumber) {
+            $("#goal-number").html("<p class=\"loser\"> You suffered a <em>very</em> uncomfortble fate...  </p>");
+            losses += 1;
+            alert("death");
+        }
+        else if (playerNumber === goalNumber) {
+            $("#goal-number").html("<p class=\"winner\"> You're Cured!!</p>");
+            wins += 1;
+            alert("You won!");
+        }
     });
 
-       $('#berry4').on('click', function(){
+    $("#berry4").on("click", function(){
         playerNumber += berry4;
-        $('.current-number').text("Your current Level of Chemical X: " + playerNumber);
-
+        $(".current-number").text("Your current Level of Chemical X: " + playerNumber);
+        if (playerNumber > goalNumber) {
+            $("#goal-number").html("<p class=\"loser\"> You suffered a <em>very</em> uncomfortble fate...  </p>");
+            losses += 1;
+            alert("death");
+        }
+        else if (playerNumber === goalNumber) {
+            $("#goal-number").html("<p class=\"winner\"> You're Cured!!</p>");
+            wins += 1;
+            alert("You won!");
+        }
     });
-     } while (playerNumber < goalNumber);
 
-     if (playerNumber === goalNumber) {
-      $('#goal-number').html('<p class="win"> YOU WON!! </p>');
-      wins++;
-     }
 
 });
+    
+      
 
